ba5b77d6fc8dd1e6d02979ff9f54641f70dd462d
Joel Allen <joelallen720@gmail.com>
2015-10-10 14:30:02 -0700

    Clean admin service.

--- public/javascripts/services/admin.js
+++ public/javascripts/services/admin.js
@@ -1,24 +1,22 @@
 var app = angular.module('angular-express');
 
-app.factory('admin', [
-  '$http',
-  function($http) {
-    var _isLoggedIn = false;
+app.factory('admin', function($http) {
+  var _isLoggedIn = false;
 
-    return {
-      isLoggedIn: function() {
-        return _isLoggedIn;
-      },
+  return {
+    isLoggedIn: function() {
+      return _isLoggedIn;
+    },
 
-      logIn: function(credentials) {
-        return $http.post('/login', credentials).success(function() {
+    logIn: function(credentials) {
+      return $http.post('/login', credentials)
+        .success(function() {
           _isLoggedIn = true;
         });
-      },
+    },
 
-      logOut: function() {
-        _isLoggedIn = false;
-      },
-    };
-  }
-]);
+    logOut: function() {
+      _isLoggedIn = false;
+    },
+  };
+});
