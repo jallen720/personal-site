6c709ab61ea059d304f0f4a3acb60de4b295e201
Joel Allen <joelallen720@gmail.com>
2015-10-12 19:49:03 -0700

    Clean post body editing area.

--- public/javascripts/controllers/postForm.js
+++ public/javascripts/controllers/postForm.js
@@ -3,18 +3,24 @@ var app = angular.module('angular-express');
 app.controller(
   'postForm',
   function($scope) {
-    var TOOLS = [
-      'sizeselect',
-      'bold italic',
-      'fontselect',
-      'fontsizeselect',
-    ].join(' | ');
-
     $scope.tinymceOptions = {
       content_css: 'stylesheets/style.css',
       menubar:     false,
       statusbar:   false,
-      toolbar:     TOOLS,
+
+      plugins: [
+        'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+        'searchreplace wordcount visualblocks visualchars code fullscreen',
+        'insertdatetime nonbreaking save table contextmenu directionality',
+        'emoticons template paste textcolor colorpicker textpattern',
+      ],
+
+      toolbar:
+        'preview | styleselect | bold italic | alignleft aligncenter alignrig' +
+        'ht alignjustify | bullist numlist outdent indent | link image | fore' +
+        'color backcolor emoticons | undo redo',
+
+      image_advtab: true,
     };
   }
 );
