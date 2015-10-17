15680a15e1698ba263ae3cf9fbdb8025e988def6
Joel Allen <joelallen720@gmail.com>
2015-10-16 17:33:57 -0700

    Add header block to form partial. Add divider partial. Show errors inside form tile. Add bio settings partial. Remove switcher form settings view.

--- public/javascripts/controllers/settings.js
+++ public/javascripts/controllers/settings.js
@@ -1,36 +1,7 @@
 app.controller('settings',
 
 function($scope) {
-  $('#change-password').hide()
 
-  function setSwitcherOn(switcher, isOn) {
-    $('#switcher-' + switcher).toggleClass('switcher-selected', isOn);
-  }
-
-  setSwitcherOn('left', true);
-
-  function updateSwitchers(isChangeEmail) {
-    setSwitcherOn('left', isChangeEmail);
-    setSwitcherOn('right', !isChangeEmail);
-  }
-
-  function offView(isChangeEmail) {
-    return isChangeEmail ? 'change-password' : 'change-email';
-  }
-
-  function fadeViews(onView, offView) {
-    const FADE_DURATION = 100;
-
-    $('#' + offView).fadeOut(FADE_DURATION, function() {
-      $('#' + onView).fadeIn(FADE_DURATION);
-    });
-  }
-
-  $scope.switchTo = function(view) {
-    var isChangeEmail = view === 'change-email';
-    updateSwitchers(isChangeEmail);
-    fadeViews(view, offView(isChangeEmail));
-  };
 }
 
 );
