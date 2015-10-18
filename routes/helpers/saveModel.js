ea936970494f7a7dbd0dfaacb3c083fc31fa9d6e
Joel Allen <joelallen720@gmail.com>
2015-10-18 12:11:04 -0700

    Clean saveModel routes helper.

--- routes/helpers/saveModel.js
+++ routes/helpers/saveModel.js
@@ -1,16 +1,12 @@
 var sendError        = require('./sendError'),
     getErrorMessages = require('../../utils/getErrorMessages');
 
-function saveCheck(res) {
-  return function(err) {
+module.exports = function(model, res) {
+  model.save(function(err) {
     if (err) {
       sendError(res, getErrorMessages(err.errors));
     } else {
       res.sendStatus(200);
     }
-  };
-}
-
-module.exports = function(model, res) {
-  model.save(saveCheck(res));
+  });
 };
