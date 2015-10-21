3cf3ee5521f6fc28eb53177ae97e4e8f0c8764bb
Joel Allen <joelallen720@gmail.com>
2015-10-20 19:24:52 -0700

    Handle configuration in main module and bootstrapping in bootstrap module.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -30,51 +30,8 @@ require.config({
       ],
     },
   },
-});
 
-require([
-  // Register modules.
-  'modules/blog',
-
-  // Run initialization scripts.
-  'initialization/smoothScroll',
-  'initialization/scrollToTop',
-
-  // Register states.
-  'states/settings',
-  'states/login',
-  'states/home',
-  'states/default',
-  'states/post/create',
-  'states/post/read',
-  'states/post/update',
-  'states/post/delete',
-
-  // Register services.
-  'services/posts',
-  'services/admin',
-
-  // Register controllers.
-  'controllers/NavigationCtrl',
-  'controllers/HomeCtrl',
-  'controllers/LoginCtrl',
-  'controllers/BioCtrl',
-  'controllers/post/CreateCtrl',
-  'controllers/post/ReadCtrl',
-  'controllers/post/UpdateCtrl',
-  'controllers/post/DeleteCtrl',
-  'controllers/post/FormCtrl',
-  'controllers/post/BodyCtrl',
-  'controllers/post/EditorCtrl',
-  'controllers/settings/BioCtrl',
-  'controllers/settings/EmailCtrl',
-  'controllers/settings/PasswordCtrl',
-  'controllers/settings/bio/TinyMceCtrl',
-],
-
-function() {
-  // Bootstrap application modules.
-  angular.bootstrap(document, [
-    'blog'
-  ]);
+  deps: [
+    'bootstrap',
+  ],
 });
