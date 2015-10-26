ce4f29cd5b5a28bdf49d40873b8f802b5cd758f2
Joel Allen <joelallen720@gmail.com>
2015-10-25 19:22:19 -0700

    Clean main Angular app script.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,22 @@
 require.config({
   paths: {
     angular:        '../lib/angular/angular.min',
+    angularMd5:     '../lib/angular-md5/angular-md5.min',
     uiRouter:       '../lib/angular-ui-router/release/angular-ui-router.min',
     tinyMce:        '../lib/tinymce/tinymce.min',
     uiTinyMce:      '../lib/angular-ui-tinymce/src/tinymce',
-    angularMd5:     '../lib/angular-md5/angular-md5.min',
     jquery:         '../lib/jquery/dist/jquery.min',
     TweenMax:       '../lib/gsap/src/minified/TweenMax.min',
     ScrollToPlugin: '../lib/gsap/src/minified/plugins/ScrollToPlugin.min',
   },
 
   shim: {
+    angularMd5: {
+      deps: [
+        'angular',
+      ],
+    },
+
     uiRouter: {
       deps: [
         'angular',
@@ -21,12 +27,6 @@ require.config({
       deps: [
         'angular',
         'tinyMce',
-      ],
-    },
-
-    angularMd5: {
-      deps: [
-        'angular',
       ],
     },
   },
