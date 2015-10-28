06fff1bc1d23d942b25fae896849662ff55c7897
Joel Allen <joelallen720@gmail.com>
2015-10-28 12:21:39 -0700

    Install bower & nodemon locally.

--- utils/loadEnvironment.js
+++ utils/loadEnvironment.js
@@ -2,7 +2,7 @@ var env2 = require('env2');
 
 module.exports = function() {
   // Load development environment configuration.
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === undefined) {
     env2('config.env');
   }
 }
