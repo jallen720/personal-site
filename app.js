0aed98c4bfc3196858c6fa045ef35ecb8d750aa7
Joel Allen <joelallen720@gmail.com>
2015-11-09 20:03:24 -0800

    Add favicon.

--- app.js
+++ app.js
@@ -25,8 +25,7 @@ mongoConnect(mongoose);
 app.set('views', 'views');
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon('public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
